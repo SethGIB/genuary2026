7444869db0aebc0dcf2533375e2207f85e914b8e
Seth Gibson <seth@goindiebro.com>
2026-01-12 19:33:47 -0700

    finished 03 instanced

--- genuary202603/include/genuary202603App.h
+++ genuary202603/include/genuary202603App.h
@@ -20,29 +20,16 @@ private:
 	int mLastNumber = 0;
 	int mNextNumber = 0;
 
-	const int kNumRows = 80;
-	const int kNumCols = 120;
-	const float kNoiseMin = 0.0f;
-	const float kNoiseMax = 0.25f;
-	const float kXScale = 0.015f;
-	const float kYScale = 0.02f;	
-	const float kXStep = 0.1f;
-	const float kYStep = 0.08f;
-
-	const vec4 kBlue = vec4(38.0f / 255.0f, 104.0f / 255.0f, 120.0f / 255.0f, 0.9f);
-	const vec4 kGreen = vec4(42.0f / 255.0f, 120.0f / 255.0f, 38.0f / 255.0f, 0.2f);
-	const vec4 kBrown = vec4(94.0f / 255.0f, 74.0f / 255.0f, 34.0f / 255.0f, 1.0f);
-	const vec4 kSky = vec4(64.0f/255.0f, 107.0f / 255.0f, 210.0f / 255.0f, 1.0f);
-	const vec4 kSnow = vec4(0.9f, 0.9f, 1.0f, 0.85f);
 
 	const int kFibboA[6] = { 3,3,6,9,15,24 };
 	const int kFibboB[6] = { 1,2,3,5,8,13 };
 
 	//Points
 	gl::BatchRef mTerrainBatch;
-	gl::BatchRef mFireworksBatch;
-	gl::VboRef mTerrainBuffer;
-	gl::VboRef mFireworksBuffer;
+	gl::VboRef mTerrainXyzVbo;
+	gl::VboRef mTerrainRgbVbo;
+	gl::VboMeshRef mTerrainShape;
+	gl::GlslProgRef mTerrainShader;
 
 	struct mPoint {
 		vec3 position;
@@ -50,17 +37,14 @@ private:
 	};
 
 	vector<mPoint> mTerrainData;
-	
-	vector<ffEmitter> mFireworksEmitters;
-	vector<mPoint> mFireworksData;
-
 	gl::TextureRef mTexBG;
 
 	CameraPersp	mCamera;
 	CameraUi	mCameraCtrl;
 
 	void setupCamera();
-	void setup3DObjects();
+	void setupShaders();
+	void setupInstancedMesh();
 	
 	void updateTerrain( float gOffset);
 
--- genuary202603/src/genuary202603App.cpp
+++ genuary202603/src/genuary202603App.cpp
@@ -3,29 +3,37 @@
 #include "cinder/gl/Vbo.h"
 #include "cinder/Rand.h"
 #include "cinder/Perlin.h"
+#include "cinder/Rand.h"
+
+const float kCellSize = 0.025f;
+const unsigned int kNumWidth = 90;
+const unsigned int kNumDepth = 160;
+
+const float kNoiseMin = -0.8f;
+const float kNoiseMax = 0.8f;
+const float kXScale = 0.015f;
+const float kYScale = 0.02f;
+const float kXStep = 0.1f;
+const float kYStep = 0.08f;
+
+const vec4 kBlue = vec4(38.0f / 255.0f, 104.0f / 255.0f, 120.0f / 255.0f, 0.9f);
+const vec4 kGreen = vec4(42.0f / 255.0f, 120.0f / 255.0f, 38.0f / 255.0f, 0.2f);
+const vec4 kBrown = vec4(94.0f / 255.0f, 74.0f / 255.0f, 34.0f / 255.0f, 1.0f);
+const vec4 kSky = vec4(64.0f / 255.0f, 107.0f / 255.0f, 210.0f / 255.0f, 1.0f);
+const vec4 kSnow = vec4(0.9f, 0.9f, 1.0f, 0.85f);
 
 void genuary202603App::setup()
 {
 	setupCamera();
-	updateTerrain(0.0f);
-	setupFireworks();
-	setup3DObjects();
-	gl::enableAlphaBlending();
+	//updateTerrain(0.0f);
+	setupInstancedMesh();
+	gl::disableAlphaBlending();
 }
 
 void genuary202603App::update()
 {
 	float t = getElapsedSeconds() * 2.2f;
 	updateTerrain(t);
-	updateFireworks();
-
-	void* bufferPtr = mTerrainBuffer->mapReplace();
-	memcpy(bufferPtr, mTerrainData.data(), mTerrainData.size() * sizeof(mPoint));
-	mTerrainBuffer->unmap();
-
-	bufferPtr = mFireworksBuffer->mapReplace();
-	memcpy(bufferPtr, mFireworksData.data(), mFireworksData.size() * sizeof(mPoint));
-	mFireworksBuffer->unmap();
 }
 
 void genuary202603App::draw()
@@ -43,11 +51,7 @@ void genuary202603App::draw()
 	gl::setMatrices(mCamera);
 	gl::enableDepthRead();
 	gl::enableDepthWrite();
-	gl::pointSize(8.0f);
-	mTerrainBatch->draw();
-
-	//gl::pointSize(4.0f);
-	//mFireworksBatch->draw();
+	mTerrainBatch->drawInstanced( kNumWidth * kNumDepth);
 
 	gl::popMatrices();
 }
@@ -65,29 +69,133 @@ void genuary202603App::setupCamera()
 	mCamera.setPerspective(verticalFOV, aspectRatio, nearClip, farClip);
 	mCamera.lookAt(cameraPosition, cameraTarget, cameraUpAxis);
 	mCameraCtrl = CameraUi(&mCamera, getWindow());
-}
-
-void genuary202603App::setup3DObjects()
-{
-	mTerrainBuffer = gl::Vbo::create(GL_ARRAY_BUFFER, mTerrainData, GL_STREAM_DRAW);
-	geom::BufferLayout layout;
-	layout.append(geom::Attrib::POSITION, 3, sizeof(mPoint), offsetof(mPoint, position));
-	layout.append(geom::Attrib::COLOR, 3, sizeof(mPoint), offsetof(mPoint, color));
-
-	auto terrainMesh = gl::VboMesh::create(static_cast<uint32_t>(mTerrainData.size()), GL_POINTS, { { layout, mTerrainBuffer } });
-	mTerrainBatch = gl::Batch::create(terrainMesh, gl::getStockShader(gl::ShaderDef().color()));
-
-	mFireworksBuffer = gl::Vbo::create(GL_ARRAY_BUFFER, mFireworksData, GL_STREAM_DRAW);
-
-	auto fireworksMesh = gl::VboMesh::create(static_cast<uint32_t>(mFireworksData.size()), GL_POINTS, { { layout, mFireworksBuffer } });
-	mFireworksBatch = gl::Batch::create(fireworksMesh, gl::getStockShader(gl::ShaderDef().color()));
 
 	mTexBG = gl::Texture::create(loadImage(loadAsset("TX_BG_Starry.png")));
 }
 
+void genuary202603App::setupShaders()
+{
+	auto vert_prog = CI_GLSL(150,
+		uniform mat4 ciModelViewProjection;
+		uniform mat3 ciNormalMatrix;
+
+		in vec4 ciPosition;
+		in vec2 ciTexCoord0;
+		in vec3 ciNormal;
+
+		// per instance
+		in vec3 instXYZ;
+		in vec3 instRGB;
+
+		out lowp vec4 Color;
+		out highp vec3 Normal;
+
+		void main(void)
+		{
+			gl_Position = ciModelViewProjection * (4.0 * ciPosition + vec4(instXYZ, 0));
+			Color = vec4(instRGB, 1.0f);
+			Normal = ciNormalMatrix * ciNormal;
+		}
+	);
+
+	auto frag_prog = CI_GLSL(150,
+		
+		in vec4	Color;
+		in vec3	Normal;
+		in vec2	TexCoord;
+
+		out vec4 oColor;
+
+		void main(void)
+		{
+			vec3 normal = normalize(vec3(-Normal.x, Normal.y, -Normal.z));
+			float diffuse = max(dot(normal, vec3(0.0, 1.0, 0.0)), 0);
+			oColor = Color * diffuse;
+		}
+	);
+
+	mTerrainShader = gl::GlslProg::create(vert_prog, frag_prog);
+}
+
+void genuary202603App::setupInstancedMesh()
+{
+	vector<vec3> rgb_data;
+	vector<vec3> xyz_data;
+
+	float halfx = (kNumWidth * kCellSize * 4.0f) * 0.5f;
+	float halfz = (kNumDepth * kCellSize * 4.0f) * 0.5f;
+	for (int z = 0; z < kNumDepth; z++)
+	{
+		float pz = -halfz + ((z+0.5f) * kCellSize * 4.0f);
+
+		for (int x = 0; x < kNumWidth; x++)
+		{
+			float px = -halfx + ((x+0.5f) * kCellSize * 4.0f);
+			xyz_data.push_back(vec3(px, 0.0f, pz));
+			rgb_data.push_back(vec3(randFloat(), randFloat(), randFloat()));
+		}
+	}
+
+	mTerrainXyzVbo = gl::Vbo::create(GL_ARRAY_BUFFER, xyz_data.size() * sizeof(vec3), xyz_data.data(), GL_DYNAMIC_DRAW);
+	geom::BufferLayout xyzLayout;
+	xyzLayout.append(geom::CUSTOM_0, 3, 0, 0, 1);
+	
+	mTerrainRgbVbo = gl::Vbo::create(GL_ARRAY_BUFFER, rgb_data.size() * sizeof(vec3), rgb_data.data(), GL_DYNAMIC_DRAW);
+	geom::BufferLayout rgbLayout;
+	rgbLayout.append(geom::CUSTOM_1, 3, 0, 0, 1);
+
+	mTerrainShape = gl::VboMesh::create(geom::Cube().size(vec3(kCellSize)).subdivisions(2));
+	mTerrainShape->appendVbo(xyzLayout, mTerrainXyzVbo);
+	mTerrainShape->appendVbo(rgbLayout, mTerrainRgbVbo);
+
+	setupShaders();
+
+	mTerrainBatch = gl::Batch::create(mTerrainShape, mTerrainShader, { {geom::Attrib::CUSTOM_0, "instXYZ"},{geom::Attrib::CUSTOM_1, "instRGB"} });
+}
+
 void genuary202603App::updateTerrain( float gOffset )
 {
-	mTerrainData.clear();
+	vec3 *pos = (vec3*)mTerrainXyzVbo->mapReplace();
+	vec3* rgb = (vec3*)mTerrainRgbVbo->mapReplace();
+	float halfx = (kNumWidth * kCellSize * 4.0f) * 0.5f;
+	float halfz = (kNumDepth * kCellSize * 4.0f) * 0.5f;
+	float yOffset = gOffset;
+
+	for (int z = 0; z < kNumDepth; z++)
+	{
+		float xOffset = 0.0f;
+		float pz = -halfz + ((z + 0.5f) * kCellSize * 4.0f);
+		for (int x = 0; x < kNumWidth; x++)
+		{
+			float px = -halfx + ((x + 0.5f) * kCellSize * 4.0f);
+			float y0 = lmap<float>(Perlin().noise(xOffset, yOffset), -0.5f, .5f, kNoiseMin, kNoiseMax);
+			y0 = glm::clamp<float>(y0, 0.07f, kNoiseMax);
+			*pos++ = vec3(px, y0, pz);
+			xOffset += kXStep;
+
+			vec3 newRgb = vec3(kBlue);
+			if (y0 > 0.07f && y0 < 0.4f)
+			{
+				newRgb = kGreen;
+			}
+			else if (y0 > 0.4f && y0 < 0.6f)
+			{
+				newRgb = kBrown;
+			}
+			else if (y0 > 0.6f)
+			{
+				newRgb = kSnow;
+			}
+
+			*rgb++ = newRgb;
+
+		}
+		yOffset += kYStep;
+	}
+	mTerrainXyzVbo->unmap();
+	mTerrainRgbVbo->unmap();
+
+	/*mTerrainData.clear();
 	float yOffset = gOffset;
 	for (int y = 0; y < kNumRows; ++y)
 	{
@@ -112,46 +220,16 @@ void genuary202603App::updateTerrain( float gOffset )
 			xOffset += kXStep;
 		}
 		yOffset += kYStep;
-	}
+	}*/
 }
 
 void genuary202603App::setupFireworks()
 {
-	mFireworksEmitters.clear();
-	for (int i = 0; i < 9; i++)
-	{
-		float ffx = Rand::randFloat(-0.3f, 0.3f);
-		float ffy = Rand::randFloat(0.4f, 1.2f);
-		vec4 color = vec4(Rand::randFloat(0.5f, 1.0f), Rand::randFloat(0.5f, 1.0f), Rand::randFloat(0.5f, 1.0f), 1.0f);
-		ffEmitter emitter = ffEmitter(6, color, vec3(ffx, ffy, 0.0f));
-		mFireworksEmitters.push_back(emitter);
-	}
-
 	updateFireworks();
 }
 
 void genuary202603App::updateFireworks()
 {
-	mFireworksData.clear();
-	for( int f=0;f < mFireworksEmitters.size(); f++ )
-	{
-		if(mFireworksEmitters[f].mAlive == false)
-		{
-			float ffx = Rand::randFloat(-0.3f, 0.3f);
-			float ffy = Rand::randFloat(0.4f, 1.2f);
-			vec4 color = vec4(Rand::randFloat(0.5f, 1.0f), Rand::randFloat(0.5f, 1.0f), Rand::randFloat(0.5f, 1.0f), 1.0f);
-			ffEmitter emitter = ffEmitter(6, color, vec3(ffx, ffy, 0.0f));
-			mFireworksEmitters[f] = emitter;
-		}
-
-		mFireworksEmitters[f].step();
-		for( int p=0; p < mFireworksEmitters[f].mParticles.size(); p++ )
-		{
-			vec3 position = mFireworksEmitters[f].mParticles[p].mPos + mFireworksEmitters[f].mOrigin;
-			vec4 color = mFireworksEmitters[f].mColor;
-			mFireworksData.push_back({ position, color });
-		}
-	}	
 }
 
 void prepareSettings( App::Settings* settings )
